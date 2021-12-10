4111697d6d8d85c84bf6ecb5b94b27a04ca5d4ba
qixiaoling <qixiaolinglinda@hotmail.com>
2021-12-10 11:12:37 +0100

    Services 2

--- src/Component/Services/Services.js
+++ src/Component/Services/Services.js
@@ -1,18 +1,22 @@
 import React, {useState} from 'react';
 import {servicesData} from "../../Assets/Services";
-
+import './Services.css'
 
 function Services() {
     const [services, setServices] = useState(servicesData)
     return (
-        <>
-            {services.map((item)=>{
-                const {icon, role, text } = item;
-                return(
-                    <article>{role}</article>
+        <section className='services-container'>
+            {services.map((item) => {
+                const {icon, role, text} = item;
+                return (
+                    <article className="service-item">
+                        <i className={icon}></i>
+                        <p>{role}</p>
+                        <p>{text}</p>
+                    </article>
                 )
             })}
-        </>
+        </section>
 
     )
 }
