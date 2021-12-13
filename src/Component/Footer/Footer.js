264810ed5d116cdb9dc21a660c26c75682fe6d27
qixiaoling <qixiaolinglinda@hotmail.com>
2021-12-13 09:54:17 +0100

    Footer css 2

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -11,7 +11,7 @@ function Footer() {
                 </LinkR>
                 <div className='footer-text'>
                     <div className='footer-left'>
-                        <h4>Subscribe</h4>
+                        <p className='footer-left-title'>Subscribe</p>
                         <p>Select topics and stay current with our latest insights</p>
                         <form className='footer-form'>
                             <input type='text' placeholder='Email address'/>
@@ -19,20 +19,37 @@ function Footer() {
                         </form>
                     </div>
                     <div className='footer-right'>
-                        <div className='footer-right-row-one'>
-                            <LinkR to='/'>Contact us</LinkR>
-                            <LinkR to='/'>FAQ</LinkR>
-                            <LinkR to='/'>Privacy policy</LinkR>
-                            <LinkR to='/'>Cookie policy</LinkR>
-                            <LinkR to='/'>Terms of use</LinkR>
+                        <div className='footer-right-links'>
+                            <div className='footer-right-row-one'>
+                                <LinkR className='footer-link' to='/'>Contact us</LinkR>
+                                <LinkR className='footer-link' to='/'>FAQ</LinkR>
+                                <LinkR className='footer-link' to='/'>Privacy policy</LinkR>
+                                <LinkR className='footer-link' to='/'>Cookie policy</LinkR>
+                                <LinkR className='footer-link' to='/'>Terms of use</LinkR>
+                            </div>
+                            <div className='footer-right-row-two'>
+                                <LinkR className='footer-link' to='/'>Local language information</LinkR>
+                                <LinkR className='footer-link' to='/'>Accessibility statement</LinkR>
+                            </div>
                         </div>
-                        <div className='footer-right-row-two'>
-                            <LinkR to='/'>Contact us</LinkR>
-                            <LinkR to='/'>FAQ</LinkR>
-                            <LinkR to='/'>Privacy policy</LinkR>
-                            <LinkR to='/'>Cookie policy</LinkR>
-                            <LinkR to='/'>Terms of use</LinkR>
+                        <div className='footer-icon'>
+                            <button className='footer-icon-btn'>
+                                <i className="fab fa-linkedin-in"></i>
+                            </button>
+                            <button className='footer-icon-btn'>
+                                <i className="fab fa-twitter"></i>
+                            </button>
+                            <button className='footer-icon-btn'>
+                                <i className="fab fa-facebook-f"></i>
+                            </button>
+                            <button className='footer-icon-btn'>
+                                <i className="fab fa-youtube"></i>
+                            </button>
+                            <button className='footer-icon-btn'>
+                                <i className="fab fa-linkedin-in"></i>
+                            </button>
                         </div>
+
                     </div>
                 </div>
             </div>
